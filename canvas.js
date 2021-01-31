d62fd711ee15ceb1a5cde8ce0ad4492dbfd37f75
karine809 <karinemesropyan1984@gmail.com>
2021-01-31 21:42:25 +0400

    Create Ball / Bee

--- canvas.js
+++ canvas.js
@@ -12,42 +12,78 @@ let circle = function(x, y, radius, fillCircle){
         }
 };
 
-let drawBee = function(x, y){
-    context.lineWidth = 2;
-    context.strokeStyle = "Black";
-    context.fillStyle = "Gold";
+// let drawBee = function(x, y){
+//     context.lineWidth = 2;
+//     context.strokeStyle = "Black";
+//     context.fillStyle = "Gold";
 
-    circle(x, y, 8, true);
-    circle(x, y, 8, false);
-    circle(x - 5, y - 11, 5, false);
-    circle(x + 5, y -11, 5, false);
-    circle(x - 2, y - 1, 2, false);
-    circle(x + 2, y - 1, 2, false);
-}; 
+//     circle(x, y, 8, true);
+//     circle(x, y, 8, false);
+//     circle(x - 5, y - 11, 5, false);
+//     circle(x + 5, y -11, 5, false);
+//     circle(x - 2, y - 1, 2, false);
+//     circle(x + 2, y - 1, 2, false);
+// }; 
 
 
-//Change coordinate of bee
-let update = function(coordinate){
-    let offset = Math.random() * 4 - 2;
-    coordinate += offset;
-    if(coordinate > 200) {
-        coordinate = 200;
-    } 
-    if (coordinate < 0) {
-        coordinate = 0;
+// //Change coordinate of bee
+// let update = function(coordinate){
+//     let offset = Math.random() * 4 - 2;
+//     coordinate += offset;
+//     if(coordinate > 200) {
+//         coordinate = 200;
+//     } 
+//     if (coordinate < 0) {
+//         coordinate = 0;
+//     }
+//     return coordinate;
+// }; 
+
+// // Global vatiables
+// let x = 100;
+// let y = 100;
+
+// setInterval(function(){
+//     context.clearRect(0, 0, 200, 200);
+//     drawBee(x, y);
+//     x = update(x);
+//     y = update(y);
+//     context.stokeRect(0, 0, 200, 200);
+// }, 50);
+
+////---------- Create Ball
+
+let Ball = function(){
+    context.fillStyle = "Red";
+    this.x = 100;
+    this.y = 100;
+    this.xSpeed = -2;
+    this.ySpeed = 3;
+};
+
+Ball.prototype.draw = function(){
+    circle(this.x, this.y, 5, true)
+};
+
+Ball.prototype.move = function(){//sharjum enq gngakin
+    this.x += this.xSpeed;
+    this.y += this.ySpeed;
+};
+Ball.prototype.checkCollision = function(){//stugum enq gndak@ xpum e paterin
+    if (this.x < 0  || this.x > 195){
+        this.xSpeed = -this.xSpeed;
     }
-    return coordinate;
-}; 
+    if (this.y < 0 || this.y > 195){
+        this.ySpeed = -this.ySpeed;
+    }
+};
 
-// Global vatiables
-let x = 100;
-let y = 100;
+let ball = new Ball();
 
 setInterval(function(){
-    context.clearRect(0, 0, 200, 200);
-    drawBee(x, y);
-    x = update(x);
-    y = update(y);
-    context.stokeRect(0, 0, 200, 200);
+    context.clearRect(0, 0, 210, 210);
+    ball.draw();
+    ball.move();
+    ball.checkCollision();
+    context.strokeRect(0, 0, 200, 200);
 }, 50);
-
